d63f1402ea23b315c0834f6bf4f6d206e10f01b6
mishean <mishean.jp@hotmail.com>
2022-01-21 15:10:56 -0500

    fixed pdf file

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,15 +1,13 @@
 import React from 'react'
-
-
+import pdf from './docs/Resume.pdf'
 
 export default function Footer() {
     return (
         <div>
             <a href="http://www.linkedin.com/in/mishean-peiris/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
             <a href="mailto:misheanpeiris@gmail.com">email</a>
-            <a href="public/Resume.pdf" download> Download </a>
+            <a href={pdf} target="_blank" rel="noreferrer">Resume</a>
             <p>Last Update: Jan 21, 2022</p>
-
         </div>
     )
 }
