4f231ac0d6df184a9fba7727be477f05f9af3f7c
mishean <mishean.jp@hotmail.com>
2022-01-21 00:30:29 -0500

    added contact form

---
+++ src/Intro.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Intro.css';
+
+
+
+export default function Intro() {
+    return (
+        <div>
+            <h3>
+                Welcome
+            </h3>
+            <p>
+                I am a Front-End Developer / UI designer specalizing in building e-commerce, portfolio and experimental art apps.
+            </p>
+        </div>
+    )
+}
---
+++ src/contact.jsx
@@ -0,0 +1,32 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+export const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_a96u9g9', 'template_psx5szc', form.current, 'user_3WG0u0eILBpstx5kc0doI')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      form.current.reset()
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+        <label>Subject</label>
+      <input type="text" name="subject" />
+      <label>Name</label>
+      <input type="text" name="name" />
+      <label>Email</label>
+      <input type="email" name="email" />
+      <label>Message</label>
+      <textarea name="message" />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
