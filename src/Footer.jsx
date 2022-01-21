7cb868850ce541e6b21625dc61f0aaf2a747d96f
mishean <mishean.jp@hotmail.com>
2022-01-21 14:14:30 -0500

    readded files

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
 
+
 export default function Footer() {
     return (
         <div>
             <a href="http://www.linkedin.com/in/mishean-peiris/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
             <a href="mailto:misheanpeiris@gmail.com">email</a>
-            <a href="./pdf/resume.zip" download> Download </a>
+            <a href="public/Resume.pdf" download> Download </a>
             <p>Last Update: Jan 21, 2022</p>
-            <img src="pdf/IMG_4270.jpg" alt="yuh" />
 
         </div>
     )
