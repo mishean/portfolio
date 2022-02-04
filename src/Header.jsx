7658be4e3d6200a8d81f46d6a270230180769ae8
mishean <mishean.jp@hotmail.com>
2022-02-04 02:04:50 -0500

    altered nav bar spaceing (space btwn)

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,16 @@
 import React, {useEffect, useState } from 'react';
 import {Link} from 'react-router-dom'
 import './Header.css'
+import {TiThMenu} from 'react-icons/ti'
+import {IoClose} from 'react-icons/io5'
 
 function Header() {
-  const [show, setShow] = useState(false)
+  const [show, setShow] = useState(false);
+  const [size, setSize] = useState(false);
+
+  const handleChange = () => {
+    setSize(!size);
+  }
     const controlNavbar = () => {
         if (window.scrollY > 2) {
             setShow(true)
@@ -23,15 +30,19 @@ function Header() {
     <div>
         <nav className={show ? 'nav nav-2' : 'nav'}>
         <Link className='logo' to="/"> MISHEAN </Link>
-        <div className='nav-item'>
+        
+          <div       className='menu-icon'onClick={handleChange}>{size ? <IoClose/> : <TiThMenu/>}
+          </div>
+          <div className={size ? 'nav-item active' : 'nav-item'}>
         <Link className='item' to="/intro"> PROFILE </Link>
-        <Link className='item'  to="/contact"> CONTACT </Link>
-        </div>
-      </nav>
+        <Link className='item'  to="/contact"> CONTACT
+          </Link>
+          </div>
+        </nav>
     </div>
   )
-  ;
 }
 
 export default Header;
+
 
